1c0cae81185b82628ce881f8010bd01044392753
marina-sviridova <marina.sviridova.w@gmail.com>
2026-04-13 21:17:53 +0200

    Initial project setup: Spring Boot, JPA, PostgreSQL, Lombok

---
+++ coinpilot/src/main/java/com/coinpilot/CoinpilotApplication.java
@@ -0,0 +1,13 @@
+package com.coinpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinpilotApplication.class, args);
+	}
+
+}
---
+++ coinpilot/src/test/java/com/coinpilot/CoinpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
